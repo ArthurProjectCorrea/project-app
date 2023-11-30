8ef551379fe71508fc518eb98848b30e33940117
Arthur de Paula Correa <arthur.correa@grupohpar.corp>
2023-11-30 11:05:58 -0400

    Install-framework: Vue.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+        laravel(['resources/js/app.js']),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    // O plugin Vue irá reescrever URLs de ativos, quando referenciados
+                    // em componentes de arquivo único, para apontar para a web do Laravel
+                    // servidor. Definir isso como `null` permite o plugin Laravel
+                    // para reescrever URLs de ativos para apontar para o Vite
+                    // servidor em vez disso.
+                    base: null,
+
+                    // O plugin Vue irá analisar URLs absolutos e tratá-los
+                    // como caminhos absolutos para arquivos no disco. Definir isso para
+                    // `false` deixará URLs absolutos intocados para que possam
+                    // referencie ativos no diretório público conforme esperado.
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
